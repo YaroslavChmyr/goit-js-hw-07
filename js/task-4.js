fa4165d088b1faef6b6f2c7f4b7c07714c867c58
Yaroslav Chmyr <yaroslav.ch72@gmail.com>
2024-06-16 13:10:59 +0300

    Task 4 done.

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  if (login === "" || password === "") {
+    return window.alert("All form fields must be filled in");
+  }
+  const formData = {
+    email: login,
+    password: password,
+  };
+  console.log(formData);
+  form.reset();
+}
+
+loginForm.addEventListener("submit", handleSubmit);
